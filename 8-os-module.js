ed647eaf19635b0b908d3d1d43bbdc3d00a5e829
mdkadolkar <mdkadolkar@gmail.com>
2023-10-10 17:11:56 +0530

    Add files via upload

---
+++ 8-os-module.js
@@ -0,0 +1,14 @@
+const os = require('os');
+// info about current user
+const user = os.userInfo();
+console.log(user);
+// info about system uptime
+console.log(`The system uptime is ${os.uptime()}`);
+
+const currentOS = {
+    name:os.type(),
+    release:os.release(),
+    totalMem : os.totalmem(),
+    freemem : os.freemem()
+}
+console.log(currentOS);
---
+++ module.js
@@ -0,0 +1,10 @@
+const names = require('./4-names')
+const sayHi = require('./5-utils');
+const { singlePerson } = require('./6-alternative-ways');
+
+const data = require('./6-alternative-ways');
+require('./7-mind-granade');
+// console.log(data.singplePerson);
+sayHi(names.john)
+sayHi(names.peter)
+sayHi('susan')
